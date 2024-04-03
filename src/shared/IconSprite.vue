ee2bb53981670192af4e79b7516aa95adb72f0cc
AndreiMakrushin <andrei18makrushin@gmail.com>
2024-04-03 19:35:29 +0500

    svg fix

--- src/shared/IconSprite.vue
+++ src/shared/IconSprite.vue
@@ -5,7 +5,7 @@ const getFilePath = (filePath: string, base?: string) =>
   new URL(filePath, base ?? import.meta.url).href
 
 const getHUDAsset = (assetName: string): string =>
-  getFilePath(`../assets/img/${assetName}`, import.meta.url)
+  getFilePath(`../../src/assets/img/${assetName}`, import.meta.url)
 
 const sprite = getHUDAsset('sprite.svg')
 
